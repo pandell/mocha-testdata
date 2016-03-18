16cf8c0b954afcd5979883f8e03c7c8dae7a76a3
Milan Gardian <mgardian@gmail.com>
2016-03-18 15:47:01 -0600

    Switch to non-ambient typings
    - Thanks @jgoz

--- index.d.ts
+++ index.d.ts
@@ -1,27 +1,52 @@
 // TypeScript typings for "mocha-testdata@1.1.3".
 
-interface MochaGiven {
-    <P1, P2, P3>(...params: { 0: P1, 1: P2, 2: P3 }[]): MochaGivenMethods3<P1, P2, P3>;
-    <P1, P2>(...params: { 0: P1, 1: P2 }[]): MochaGivenMethods2<P1, P2>;
-    <P1>(...params: P1[]): MochaGivenMethods1<P1>;
-}
-
-interface MochaGivenMethods3<P1, P2, P3> {
-    it(title: string, test: (p1: P1, p2: P2, p3: P3) => void): any;
-    test(title: string, test: (p1: P1, p2: P2, p3: P3) => void): any;
-}
-
-interface MochaGivenMethods2<P1, P2> {
-    it(title: string, test: (p1: P1, p2: P2) => void): any;
-    test(title: string, test: (p1: P1, p2: P2) => void): any;
-}
-
-interface MochaGivenMethods1<P1> {
-    it(title: string, test: (p1: P1) => void): any;
-    test(title: string, test: (p1: P1) => void): any;
-}
-
 declare module "mocha-testdata" {
-    const given: MochaGiven;
-    export = given;
+
+    // for synchronous tests
+
+    interface MochaGivenMethods3<P1, P2, P3> {
+        it(title: string, test: (p1: P1, p2: P2, p3: P3) => void): any;
+        test(title: string, test: (p1: P1, p2: P2, p3: P3) => void): any;
+    }
+
+    interface MochaGivenMethods2<P1, P2> {
+        it(title: string, test: (p1: P1, p2: P2) => void): any;
+        test(title: string, test: (p1: P1, p2: P2) => void): any;
+    }
+
+    interface MochaGivenMethods1<P1> {
+        it(title: string, test: (p1: P1) => void): any;
+        test(title: string, test: (p1: P1) => void): any;
+    }
+
+    export function given<P1, P2, P3>(first: { 0: P1, 1: P2, 2: P3 } | { 0: P1, 1: P2, 2: P3 }[], ...params: { 0: P1, 1: P2, 2: P3 }[]): MochaGivenMethods3<P1, P2, P3>;
+    export function given<P1, P2>(first: { 0: P1, 1: P2 } | { 0: P1, 1: P2 }[], ...params: { 0: P1, 1: P2 }[]): MochaGivenMethods2<P1, P2>;
+    export function given<P1>(params: P1[]): MochaGivenMethods1<P1>;
+    export function given<P1>(...params: P1[]): MochaGivenMethods1<P1>;
+
+
+    // for asynchronous tests
+
+    type DoneHandler = { (): void };
+
+    interface MochaGivenAsyncMethods3<P1, P2, P3> {
+        it(title: string, test: (done: DoneHandler, p1: P1, p2: P2, p3: P3) => void): any;
+        test(title: string, test: (done: DoneHandler, p1: P1, p2: P2, p3: P3) => void): any;
+    }
+
+    interface MochaGivenAsyncMethods2<P1, P2> {
+        it(title: string, test: (done: DoneHandler, p1: P1, p2: P2) => void): any;
+        test(title: string, test: (done: DoneHandler, p1: P1, p2: P2) => void): any;
+    }
+
+    interface MochaGivenAsyncMethods1<P1> {
+        it(title: string, test: (done: DoneHandler, p1: P1) => void): any;
+        test(title: string, test: (done: DoneHandler, p1: P1) => void): any;
+    }
+
+    export function givenAsync<P1, P2, P3>(first: { 0: P1, 1: P2, 2: P3 } | { 0: P1, 1: P2, 2: P3 }[], ...params: { 0: P1, 1: P2, 2: P3 }[]): MochaGivenAsyncMethods3<P1, P2, P3>;
+    export function givenAsync<P1, P2>(first: { 0: P1, 1: P2 } | { 0: P1, 1: P2 }[], ...params: { 0: P1, 1: P2 }[]): MochaGivenAsyncMethods2<P1, P2>;
+    export function givenAsync<P1>(params: P1[]): MochaGivenAsyncMethods1<P1>;
+    export function givenAsync<P1>(...params: P1[]): MochaGivenAsyncMethods1<P1>;
+
 }
